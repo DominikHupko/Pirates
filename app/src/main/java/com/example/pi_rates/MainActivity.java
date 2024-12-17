6934dd18491b7e7ef3fac1f72fcf22b52c2138bb
Csernák Fábián <26222007@vts.su.ac.rs>
2024-12-17 15:14:16 +0100

    Avatar
    Avatart kilehet valasztani, elmenti sharedpereferencesben és a kezdőoldalon megjelenik a kép

--- app/src/main/java/com/example/pi_rates/MainActivity.java
+++ app/src/main/java/com/example/pi_rates/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.pi_rates;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -120,5 +122,32 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("USER_NAME", userName);
         startActivity(intent);
         finish();
+    }
+    public void openSettings(View view) {
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        intent.putExtra("USER_NAME", userName);
+        startActivity(intent);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateAvatar();
+    }
+
+    private void updateAvatar() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String selectedAvatar = sharedPreferences.getString("SELECTED_AVATAR", "default_avatar");
+
+
+        ImageView avatarImageView = findViewById(R.id.imageViewAvatar);
+
+        if (selectedAvatar.equals("avatar1")) {
+            avatarImageView.setImageResource(R.drawable.avatar1);
+        } else if (selectedAvatar.equals("avatar2")) {
+            avatarImageView.setImageResource(R.drawable.avatar2);
+        } else {
+            avatarImageView.setImageResource(R.drawable.avatar);
+        }
     }
 }
