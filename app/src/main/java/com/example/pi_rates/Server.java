de7c3bc5f149e90f729e6ee545d4d1b6b22ff542
Robert Varro <robertvarro12@gmail.com>
2024-12-17 17:57:19 +0100

    Server link frissitese

--- app/src/main/java/com/example/pi_rates/Server.java
+++ app/src/main/java/com/example/pi_rates/Server.java
@@ -16,7 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Server {
-    public static String getURL() {return"https://2aa0-109-245-34-185.ngrok-free.app"; }
+    public static String getURL() {return"https://6f99-109-245-32-48.ngrok-free.app"; }
     private Context context;
     public Server(Context context){
         this.context = context;
